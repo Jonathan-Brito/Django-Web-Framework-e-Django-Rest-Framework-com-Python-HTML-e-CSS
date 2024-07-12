22c13ad4d23e660d2d054851872028c28d6c909d
Jonathan Brito Costa <jonathan.costa@itsgroup.com.br>
2024-07-12 08:57:45 -0300

    Implementando cards e separando o código

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.http import HttpResponse
+
+
+def my_view(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', my_view),
+    path('', include('recipes.urls'))
 ]
